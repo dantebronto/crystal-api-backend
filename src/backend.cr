172fad8e101e5aacff075a0fd6e0741cdd3d2fbe
Kellen Presley <klpresley@gmail.com>
2016-07-27 23:28:45 -0400

    config on boot - don’t serve static, allow port ENV var

--- src/backend.cr
+++ src/backend.cr
@@ -1,4 +1,7 @@
 require "../config/environment"
 
-Kemal.config.port = ENV["PORT"].to_i
-Kemal.run
+Kemal.config.tap do |c|
+  c.port = ENV["PORT"].to_i
+  c.serve_static = false
+end
+Kemal.run unless ENV["ENV"] == "test"
